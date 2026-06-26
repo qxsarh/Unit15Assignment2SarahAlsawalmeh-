c4e8e8dab55156d7a2ccc32c64d226749065bed4
qxsarh <d2pnmj2s5v@privaterelay.appleid.com>
2026-06-26 10:19:03 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,12 @@
-function sayHi() {
+function validateForm() {
+let name = document.getElementById("name").value;
+let date = document.getElementById("date").value;
 
-    alert("Hi!");
+if (name === "" || date === "") {
+alert("Please fill in all fields");
+return false;
+}
+
+alert("Booking successful!");
+return true;
 }
